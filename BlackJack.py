62bf8bff2737dc9227f848e3bf111b3839eef7d2
dkroo <dkroofee@mail.ru>
2020-04-21 15:49:53 +0500

    + переназначил __str__

--- BlackJack.py
+++ BlackJack.py
@@ -17,3 +17,11 @@ class Hand:
         if result + aces * 10 <= 21:   # решаем считать тузы за 1 очко или за 11
             result += aces * 10
         return result
+
+
+    def __str__(self):
+        text = "Рука %sа:\n" % self.name
+        for card in self.cards:
+            text += str(card) + " "
+        text += "\nКоличество очков: " + str(self.get_value())
+        return text
