db4805d7ee67656e6c6bc408ca7740d0ab941b09
dkroo <dkroofee@mail.ru>
2020-04-21 15:48:05 +0500

    + функция get_value(ценность руки)

--- BlackJack.py
+++ BlackJack.py
@@ -5,3 +5,15 @@ class Hand:
 
     def add_card(self, card):
         self.cards.append(card)    # добавить карту в руку
+
+
+    def get_value(self):   # начисление очков
+        result = 0
+        aces = 0   # количество тузов на руке
+        for card in self.cards:
+            result += card.card_value()
+            if card.get_rank() == "A":   # если на руке есть туз - увеличиваем количество тузов
+                aces += 1
+        if result + aces * 10 <= 21:   # решаем считать тузы за 1 очко или за 11
+            result += aces * 10
+        return result
