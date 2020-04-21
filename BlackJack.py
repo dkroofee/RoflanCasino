5ae9d3720cac2010a53b58ff43843370c5507b41
dkroo <dkroofee@mail.ru>
2020-04-21 15:47:09 +0500

    + функция add_card(добавить карту)

--- BlackJack.py
+++ BlackJack.py
@@ -2,3 +2,6 @@ class Hand:
     def __init__(self, name):
         self.name = name
         self.cards = []    # пустая рука
+
+    def add_card(self, card):
+        self.cards.append(card)    # добавить карту в руку
