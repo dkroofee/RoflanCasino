8742672b9a0eef28a5bb889d04814d9286789e2f
dkroo <dkroofee@mail.ru>
2020-04-21 15:42:26 +0500

    + Функция card_value(ценность карты)

--- BlackJack.py
+++ BlackJack.py
@@ -2,3 +2,9 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+
+    def card_value(self):
+        if self.rank in "TJQK":    # ценность карты
+            return 10
+        else:
+            return " A23456789".index(self.rank)
